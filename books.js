f5a459eb53c6a843f08da4542cd3a5b3e0fde202
Kyle Hochdoerfer <kjhoch2@gmail.com>
2023-10-05 22:50:55 -0400

    Modified final queries

--- books.js
+++ books.js
@@ -1,4 +1,4 @@
-/**
+/*
 	Title: books.js
     Author: Kyle Hochdoerfer, James Harper, Laurel Condon
     Date: 27 September 2023
@@ -138,3 +138,35 @@ console.log("\n")
 
 console.log("Find by bookId:")
 console.log(db.books.find({ "bookId" : "1003" })); //Lists books bookId
+console.log('')
+
+//Week 9
+const exampleId = "c001"; // Replace with desired customer
+
+//Query to display wishlist for the desired customer
+console.log(`Wishlist for Customer ID ${exampleId}`);
+console.log((db.customers.findOne({ "customerId": exampleId })).wishlistItems)
+
+//Query to add book5 to a customer wishlist
+const result = db.customers.updateOne(
+	{ customerId: exampleId },
+	{ $push: { wishlistItems: book5 } }
+);
+
+//Output the customer with the updated wishlist to demonstrate that it was updated
+console.log('')
+console.log(`Book 5 has been added to the customer with an ID of ${exampleId}`)
+console.log((db.customers.findOne({ "customerId": exampleId })).wishlistItems)
+
+const bookIdToRemove = "1005"; // Replace with desired bookId
+
+///Query to remove a book for a customer wishlist
+const removeResult = db.customers.updateOne(
+	{ customerId: exampleId },
+	{ $pull: { wishlistItems: { bookId: bookIdToRemove } } }
+);
+
+//Output the customer who had a book removed from their wishlist to demonstrate that it was deleted
+console.log('')
+console.log(`Book with an ID of ${bookIdToRemove} has been deleted`)
+console.log((db.customers.findOne({ "customerId": exampleId })).wishlistItems)
